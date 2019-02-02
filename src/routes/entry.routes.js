2675351e83383f8b9fedb30a6d44f242d0cdb486
Sean Powell <ssppowell@gmail.com>
2019-02-01 20:08:09 -0500

    IT-3 #time 1h #comment Hooking up delete service

--- src/routes/entry.routes.js
+++ src/routes/entry.routes.js
@@ -7,9 +7,35 @@ const chalk = require('chalk');
 
 const ENTRY = require('../controllers/entry.controller');
 
-router.post('/', passport.authenticate('bearer', { session: true }), ENTRY.addEntry);
-router.get('/', passport.authenticate('bearer', { session: true }), ENTRY.getAllEntries);
-router.get('/:entryID', passport.authenticate('bearer', { session: true }), ENTRY.getEntryByID);
-router.put('/:entryID', passport.authenticate('bearer', { session: true }), ENTRY.updateEntryByID);
+// NEW ENTRY
+router.post(
+  '/',
+  passport.authenticate('bearer', { session: true }),
+  ENTRY.addEntry
+);
+// GET ALL ENTRIES
+router.get(
+  '/',
+  passport.authenticate('bearer', { session: true }),
+  ENTRY.getAllEntries
+);
+// GET ENTRY BY ID
+router.get(
+  '/:entryID',
+  passport.authenticate('bearer', { session: true }),
+  ENTRY.getEntryByID
+);
+// UPDATE ENTRY BY ID
+router.put(
+  '/:entryID',
+  passport.authenticate('bearer', { session: true }),
+  ENTRY.updateEntryByID
+);
+// DELETE ENTRY BY ID
+router.delete(
+  '/:entryID',
+  passport.authenticate('bearer', { session: true }),
+  ENTRY.deleteEntryByID
+);
 
 module.exports = router;
