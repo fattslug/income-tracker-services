2fee5a0466141c4c6f4652b21af59121a98901e2
Sean Powell <ssppowell@gmail.com>
2020-01-11 15:29:56 -0500

    Adding payment methods and migration

---
+++ db/migrations.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+require('dotenv').config();
+
+const Entry = require('../src/schema/entry.schema');
+
+async function updateEntries(req, res) {
+  console.log(chalk.black.bgBlue('Updating Entries...'));
+
+  try {
+    return Entry.updateMany({
+      $or: [
+        { AmountPaid: { $gt: 0 } },
+        { PaymentType: { $exists: true } }
+      ]
+    }, [{
+      $set: {
+        "PaymentMethods": [{
+          "AmountPaid": "$AmountPaid",
+          "PaymentType": "$PaymentType"
+        }]
+      }
+    }, {
+      $unset: ["AmountPaid", "PaymentType", "Tip"]
+    }]).exec();
+  } catch (e) {
+    console.log('Error: ', e);
+  }
+}
+
+mongoose.connect(process.env.MONGO_URL, async (err) => {
+  if (err) {
+    console.log(chalk.black.bgRed('Error connecting to database: '), err);
+  } else {
+    console.log(chalk.green('Successfully connected to MongoDB'));
+    await updateEntries();
+    console.log('Finished!');
+    return process.exit(0);
+  }
+});
