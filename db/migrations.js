ca563c860fe519ca6b56a6432730e5cd93d283bd
Sean Powell <ssppowell@gmail.com>
2020-01-11 16:16:38 -0500

    Feature/payment methods array (#21)
    * Debugging
    
    * Debugging
    
    * Retrying migration

--- db/migrations.js
+++ db/migrations.js
@@ -15,16 +15,15 @@ async function updateEntries(req, res) {
         { AmountPaid: { $gt: 0 } },
         { PaymentType: { $exists: true } }
       ]
-    }, [{
+    }, {
       $set: {
         "PaymentMethods": [{
           "AmountPaid": "$AmountPaid",
           "PaymentType": "$PaymentType"
         }]
-      }
-    }, {
+      },
       $unset: ["AmountPaid", "PaymentType", "Tip"]
-    }]).exec();
+    }).exec();
   } catch (e) {
     console.log('Error: ', e);
   }
@@ -35,6 +34,7 @@ console.log('Mongo URL:', process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL, async (err) => {
   if (err) {
     console.log(chalk.black.bgRed('Error connecting to database: '), err);
+    return process.exit(22);
   } else {
     console.log(chalk.green('Successfully connected to MongoDB'));
     await updateEntries();
