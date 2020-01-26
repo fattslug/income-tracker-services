9512bb2c1c933cee6c1f1e304b35c3015e48181e
Sean Powell <ssppowell@gmail.com>
2020-01-25 16:22:38 -0800

    Fixing date range

--- src/controllers/entry.controller.js
+++ src/controllers/entry.controller.js
@@ -62,7 +62,7 @@ function getEntries(req, res) {
 
   query.DateAdded = {
     $lte: new Date(moment(parseInt(t) || new Date()).endOf('day').format()),
-    $gte: new Date(f ? moment(parseInt(f)).startOf('day').format() : moment().subtract(20, 'days').startOf('day').format())
+    $gte: new Date(f ? moment(parseInt(f)).startOf('day').format() : moment().subtract(1000, 'days').startOf('day').format())
   }
 
   if (pm) {
