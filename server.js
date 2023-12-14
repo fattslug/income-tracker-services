a8ae6965fbdb0dc3fd7519962e50a409d5ed43cc
Sean Powell <ssppowell@gmail.com>
2023-12-13 20:29:31 -0800

    update certs

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const port = process.env.PORT || 3001;
-const bodyParser = require('body-parser');
-const http = require('http');
-const https = require('https');
-const cors = require('cors');
-const passport = require('passport');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const chalk = require('chalk');
-const fs = require('fs');
+const bodyParser = require("body-parser");
+const http = require("http");
+const https = require("https");
+const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const chalk = require("chalk");
+const fs = require("fs");
 
-const db = require('./src/db');
+const db = require("./src/db");
 const app = express();
 
 app.use(passport.initialize());
 
 // CORS Setup
-if (process.env.USE_CORS === 'true') {
+if (process.env.USE_CORS === "true") {
   const allowedOrigins = process.env.ALLOWED_ORIGINS;
   const corsOptions = {
-    origin: '',
-    optionsSuccessStatus: 200
-  }
+    origin: "",
+    optionsSuccessStatus: 200,
+  };
 
   app.use((req, res, next) => {
     if (allowedOrigins.indexOf(req.headers.origin) > -1) {
       corsOptions.origin = req.headers.origin;
       next();
     } else {
-      console.log(chalk.red('Access from invalid origin: '), req.headers.origin);
+      console.log(
+        chalk.red("Access from invalid origin: "),
+        req.headers.origin
+      );
       res.sendStatus(403);
     }
   });
@@ -38,53 +41,62 @@ if (process.env.USE_CORS === 'true') {
 }
 
 // Body Parser
-app.use(bodyParser.json({
-  limit: process.env.JSON_SIZE_LIMIT
-}));
-app.use(bodyParser.urlencoded({
-  limit: process.env.JSON_SIZE_LIMIT,
-  extended: true
-}));
+app.use(
+  bodyParser.json({
+    limit: process.env.JSON_SIZE_LIMIT,
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: process.env.JSON_SIZE_LIMIT,
+    extended: true,
+  })
+);
 
 // Sessions
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  name: process.env.SESSION_NAME,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: false
-  },
-  resave: false,
-  store: new MongoStore({
-    mongooseConnection: db.connection,
-    ttl: ((60 * 60) * 12) // 12 hours
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    name: process.env.SESSION_NAME,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+    },
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: db.connection,
+      ttl: 60 * 60 * 12, // 12 hours
+    }),
   })
-}))
+);
 
 // Routes
-const entryRoutes = require('./src/routes/entry.routes');
-app.use('/entries', entryRoutes);
+const entryRoutes = require("./src/routes/entry.routes");
+app.use("/entries", entryRoutes);
 
-const paymentTypeRoutes = require('./src/routes/payment-type.routes');
-app.use('/payment_types', paymentTypeRoutes);
+const paymentTypeRoutes = require("./src/routes/payment-type.routes");
+app.use("/payment_types", paymentTypeRoutes);
 
-const authRoutes = require('./src/routes/auth.routes');
-app.use('/auth', authRoutes);
+const authRoutes = require("./src/routes/auth.routes");
+app.use("/auth", authRoutes);
 
 // Listen
 let server;
-if (process.env.USE_HTTPS == 'true') {
-  const CERT_PATH = "../certs/seanpowell-dev";
-  server = https.createServer({
-    key: fs.readFileSync(`${CERT_PATH}/seanpowell.dev.key`),
-    cert: fs.readFileSync(`${CERT_PATH}/${process.env.CERT_HASH}.crt`),
-    ca: [fs.readFileSync(`${CERT_PATH}/ca-1.crt`), fs.readFileSync(`${CERT_PATH}/ca-2.crt`), fs.readFileSync(`${CERT_PATH}/ca-3.crt`)]
-  }, app);
+if (process.env.USE_HTTPS == "true") {
+  const CERT_PATH = "/etc/letsencrypt/live/jessicadoeshair.com";
+  server = https.createServer(
+    {
+      key: fs.readFileSync(`${CERT_PATH}/privkey.pem`),
+      cert: fs.readFileSync(`${CERT_PATH}/cert.pem`),
+      ca: fs.readFileSync(`${CERT_PATH}/chain.pem`),
+    },
+    app
+  );
 } else {
   server = http.createServer(app);
 }
 server.listen(port);
-server.on('listening', () => {
-  console.log(chalk.black.bgGreen(`==> Listening on port ${port}...`))
+server.on("listening", () => {
+  console.log(chalk.black.bgGreen(`==> Listening on port ${port}...`));
 });
